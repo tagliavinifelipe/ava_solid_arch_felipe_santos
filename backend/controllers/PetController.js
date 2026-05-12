2bdb1949c00644cd6bfd58d855d90b1f4fd61898
 <tagliavinifelipe@gmail.com>
2026-05-12 17:16:15 -0300

    Task 7:
    Iplementação do schedule
    
    Iplementação do concludeAdoption
    
    Dono não pode agendar para próprio pet
    
    Só o dono pode concluir adoção

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -206,4 +206,84 @@ module.exports = class PetController {
         }
     }
        
+
+    static async schedule(req,res){
+        const id = req.params.id
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        try{
+            const pet = await Pet.findById(id)
+            if(!pet){
+                res.status(404).json({message: 'Petnão encontrado'})
+                return
+            }
+
+            if (pet.user._id.toString() === user._id.toString()){
+                res.status(403).json({message: 'você não pode agendar uma visita para seu próprio pet'})
+                return
+            }
+
+            if (!pet.available){
+            res.status(403).json({message: 'Este pet foi adotado'})
+            return
+            }
+
+            pet.adopter = {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                phone: user.phone
+            }
+
+            await pet.save()
+
+            res.status(200).json({
+                message: 'visita agendada com sucesso, entre em contato com o dono do pet.',
+                pet:pet
+            })
+        }catch(error){
+            res.status(500).json({message: error})
+        }
+    }
+
+    static async concludeAdoption(req, res){
+        const id = req.params.id
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+        
+        try{
+            const pet = await Pet.findById(id)
+
+            if(!pet){
+                res.status(404).json({message: 'Pet não encontrado'})
+                return
+            }
+
+            if(pet.user._id.toString() !== user._id.toString()){
+                res.status(403).json({message: 'Apenas o dono pode concluir a adoção'})
+                return
+            }
+
+            if(!pet.available){
+                res.status(403).json({message: 'O pet ja foi adotado'})
+                return
+            }
+        
+            if(!pet.adopter){
+                res.status(403).json({message: 'Não ha um adotante agendado para este pet'})
+                return
+            }
+        
+            pet.available = false
+            await pet.save()
+
+            res.status(200).json({
+                message: 'Adoção concluída com sucesso',
+                pet:pet
+            })
+         } catch(error){
+            res.status(500).json({message: error})
+         }
+    }
 }
